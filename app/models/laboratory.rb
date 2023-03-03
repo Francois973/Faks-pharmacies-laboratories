bd2c91f32dbed10ca19a5936c056ec7b5b50cb83
Francois973 <robino.francois973@gmail.com>
2023-03-03 18:37:27 +0100

    chore: create seed for laboratories

--- app/models/laboratory.rb
+++ app/models/laboratory.rb
@@ -1,2 +1,3 @@
 class Laboratory < ApplicationRecord
+  validates :name, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Laboratory.destroy_all
+
+Laboratory.create(name: 'Pfizer-Wyeth')
+Laboratory.create(name: 'Merck & Co')
+Laboratory.create(name: 'Abbott')
+Laboratory.create(name: 'Johnson & Johnson')
+Laboratory.create(name: 'Bristol-Mers-Squibb')
+Laboratory.create(name: 'Eli Lilly')
+Laboratory.create(name: 'Amgen')
+Laboratory.create(name: 'Sanofi-Aventis')
